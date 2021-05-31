618813b3c095614b8b830010a97b5c3544804ab0
su <wkdlfflxh@naver.com>
2021-05-31 23:32:15 +0900

    push

---
+++ week_20/14502.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+void output()
+{
+
+}
+
+void solution()
+{
+
+}
+
+void input()
+{
+
+}
+
+void preset()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+}
+
+int main(){
+	preset();
+	input();
+	solution();
+	output();
+}
